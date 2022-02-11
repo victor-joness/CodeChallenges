fc9330324e29c7d7c99fa116c2a3eb94bd05f0ae
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-02-11 08:58:25 -0300

    Update Script de algoritmo básico.js

--- FreeCodeCamp/Algoritmos JavaScript e Estruturas de Dados/Script de algoritmo básico.js
+++ FreeCodeCamp/Algoritmos JavaScript e Estruturas de Dados/Script de algoritmo básico.js
@@ -18,3 +18,33 @@ function reverseString(str) {
 console.log(reverseString("hello"));
 
 //Fatorar um número
+function factorialize(num) {
+    let resultado = 1;
+    for(let i = num; i > 1; i--){
+        resultado = resultado * i;
+    }    
+    return resultado
+}  
+console.log(factorialize(5));
+
+//Encontre a palavra mais longa em uma string
+
+function findLongestWordLength(str) {
+    let matriz = str.split(' ');
+    let maxTamanho = 0;
+
+    for(let i = 0; i < matriz.length; i++){
+        if(matriz[i].length > maxTamanho){
+            maxTamanho = matriz[i].length;
+        }
+    }
+    return maxTamanho;
+}
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+
+//Retornar os maiores números em arrays
+function largestOfFour(arr) {
+    
+}
+
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
