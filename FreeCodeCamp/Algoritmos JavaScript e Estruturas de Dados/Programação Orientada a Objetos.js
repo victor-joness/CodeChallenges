0118cf5a36e07ecfee94a2b75cfc29127d37c451
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-02-13 08:54:25 -0300

    Update Programação Orientada a Objetos.js

--- FreeCodeCamp/Algoritmos JavaScript e Estruturas de Dados/Programação Orientada a Objetos.js
+++ FreeCodeCamp/Algoritmos JavaScript e Estruturas de Dados/Programação Orientada a Objetos.js
@@ -68,5 +68,52 @@ function House(numBedrooms) {
 let myHouse = new House(4);
 console.log(myHouse instanceof House);
 
-//Compreender Propriedades Próprias
+//Compreender Propriedades Próprias, Adicione as próprias propriedades de canaryà matriz ownProps.
+
+function Bird(name) {
+    this.name = name;
+    this.numLegs = 2;
+  }
   
+  let canary = new Bird("Tweety");
+  let ownProps = [];
+  
+  for (let property in canary) {
+    if(canary.hasOwnProperty(property)) {
+      ownProps.push(property);
+    }
+  }
+  
+//Use as propriedades do protótipo para reduzir o código duplicado, Como numLegsprovavelmente terá o mesmo valor para todas as instâncias de Bird, você terá essencialmente 
+//uma variável duplicada numLegsdentro de cada Birdinstância. para acabar com isso usamos o prototype, definindo de uma forma geral todos daquela classe.
+//eai definimos isso fora do escopo da classe
+function Dog(name) {
+    this.name = name;
+}
+Dog.prototype.numLegs = 1
+let beagle = new Dog("Snoopy");
+//agora todos os derivados de dog e os propios objetos de dog tem a numLegs definida em 1, sem necessidade de duplicar informação
+
+//Iterar sobre todas as propriedades, Aqui está como você adiciona duckas próprias propriedades ao array ownPropse prototypeas propriedades ao array prototypeProps:
+
+function Bird(name) {
+    this.name = name;  //own property
+}
+Bird.prototype.numLegs = 2; // prototype property
+let duck = new Bird("Donald");
+
+let ownProps2 = [];
+let prototypeProps = [];
+
+for (let property in duck) {
+  if(duck.hasOwnProperty(property)) {
+    ownProps2.push(property);
+  } else {
+    prototypeProps.push(property);
+  }
+}
+
+console.log(ownProps2);
+console.log(prototypeProps);
+
+//Entenda a Propriedade do Construtor,
