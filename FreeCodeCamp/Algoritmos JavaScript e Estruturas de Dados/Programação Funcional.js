1d575967acb51e6ade0caf070dd07d0e0aa0680a
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-02-14 17:33:20 -0300

    Update Programação Funcional.js

--- FreeCodeCamp/Algoritmos JavaScript e Estruturas de Dados/Programação Funcional.js
+++ FreeCodeCamp/Algoritmos JavaScript e Estruturas de Dados/Programação Funcional.js
@@ -119,3 +119,25 @@ function incrementer(num) {
 console.log(incrementer(fixedValue));
 
 //Refatorar variáveis ​​globais fora de funções
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+function add(arr, bookName) {
+  let newArr = [...arr]; // Copy the bookList array to a new array.
+  newArr.push(bookName); // Add bookName parameter to the end of the new array.
+  return newArr; // Return the new array.
+}
+
+function remove(arr, bookName) {
+  let newArr = [...arr]; // Copy the bookList array to a new array.
+  if (newArr.indexOf(bookName) >= 0) {
+    // Check whether the bookName parameter is in new array.
+    newArr.splice(newArr.indexOf(bookName), 1); // Remove the given paramater from the new array.
+    return newArr; // Return the new array.
+  }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
