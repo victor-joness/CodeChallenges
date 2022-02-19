e1de68a60281a22499d9635ea061496f8d62954b
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-02-19 15:26:41 -0300

    Update Script de algoritmo intermediário.js

--- FreeCodeCamp/Algoritmos JavaScript e Estruturas de Dados/Script de algoritmo intermediário.js
+++ FreeCodeCamp/Algoritmos JavaScript e Estruturas de Dados/Script de algoritmo intermediário.js
@@ -283,3 +283,17 @@ console.log(convertHTML("Dolce & Gabbana"));
 console.log(convertHTML("Schindler's List"))
 
 //Soma todos os números ímpares de Fibonacci
+function sumFibs(num) {
+  let prevNumber = 0;
+  let currNumber = 1;
+  let result = 0;
+  while (currNumber <= num) {
+    if (currNumber % 2 !== 0) {
+      result += currNumber;
+    }
+    currNumber += prevNumber;
+    prevNumber = currNumber - prevNumber;
+  }
+
+  return result;
+}
