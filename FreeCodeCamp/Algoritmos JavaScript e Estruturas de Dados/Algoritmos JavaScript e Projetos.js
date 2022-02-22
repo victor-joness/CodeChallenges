d03c2d89859d81b03fac2c7595484b820d7dde83
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-02-22 10:12:16 -0300

    Update Algoritmos JavaScript e Projetos.js

--- FreeCodeCamp/Algoritmos JavaScript e Estruturas de Dados/Algoritmos JavaScript e Projetos.js
+++ FreeCodeCamp/Algoritmos JavaScript e Estruturas de Dados/Algoritmos JavaScript e Projetos.js
@@ -160,3 +160,60 @@ function convertToRoman(num) {
     }
    return roman;
   }
+
+//Cifra de César
+/* Uma das cifras mais simples e conhecidas é a cifra de César , também conhecida como cifra de deslocamento . Em uma cifra de deslocamento, os significados das letras são deslocados por uma determinada quantidade.
+
+Um uso moderno comum é a cifra ROT13 , onde os valores das letras são deslocados em 13 casas. Assim A ↔ N, B ↔ Oe assim por diante.
+
+Escreva uma função que receba uma string codificada em ROT13 como entrada e retorne uma string decodificada.
+
+Todas as letras serão maiúsculas. Não transforme nenhum caractere não alfabético (ou seja, espaços, pontuação), mas passe-os adiante. */
+
+function rot13(str) { // LBH QVQ VG!
+    const Acode='A'.charCodeAt();
+    const Ncode='N'.charCodeAt();
+    const Zcode='Z'.charCodeAt();
+    return [...str].map(
+      function(e){
+        const code=e.charCodeAt();
+        if(Acode<=code && code<=Zcode){
+          if(code<Ncode){
+            return String.fromCharCode(code+13);
+          } else {
+            return String.fromCharCode(code-13);
+          }
+        } else {
+          return e;
+        }
+      }
+    ).join("");
+  }
+
+  //outra solução
+
+  var lookup = {
+    'A': 'N','B': 'O','C': 'P','D': 'Q',
+    'E': 'R','F': 'S','G': 'T','H': 'U',
+    'I': 'V','J': 'W','K': 'X','L': 'Y',
+    'M': 'Z','N': 'A','O': 'B','P': 'C',
+    'Q': 'D','R': 'E','S': 'F','T': 'G',
+    'U': 'H','V': 'I','W': 'J','X': 'K',
+    'Y': 'L','Z': 'M'
+  };
+  
+  function rot13(encodedStr) {
+    var codeArr = encodedStr.split("");  // String to Array
+    var decodedArr = []; // Your Result goes here
+    // Only change code below this line
+  
+    decodedArr = codeArr.map(function(letter) {
+      if(lookup.hasOwnProperty(letter)) {
+        letter = lookup[letter];
+      }
+      return letter;
+    });
+  
+    // Only change code above this line
+    return decodedArr.join(""); // Array to String
+  }
