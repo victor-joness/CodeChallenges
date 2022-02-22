5c11ce5a88f95a13a3690182fb526e46fdddc2ce
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-02-22 10:20:57 -0300

    Update Algoritmos JavaScript e Projetos.js

--- FreeCodeCamp/Algoritmos JavaScript e Estruturas de Dados/Algoritmos JavaScript e Projetos.js
+++ FreeCodeCamp/Algoritmos JavaScript e Estruturas de Dados/Algoritmos JavaScript e Projetos.js
@@ -217,3 +217,283 @@ function rot13(str) { // LBH QVQ VG!
     // Only change code above this line
     return decodedArr.join(""); // Array to String
   }
+
+//Validador de número de telefone
+/* Retorne truese a string passada parecer um número de telefone válido dos EUA.
+
+O usuário pode preencher o campo do formulário da maneira que quiser, desde que tenha o formato de um número americano válido. 
+Veja a seguir exemplos de formatos válidos para números dos EUA (consulte os testes abaixo para outras variantes):
+
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555
+Para este desafio, você receberá uma string como 800-692-7753ou 8oo-six427676;laskdjf. Seu trabalho é validar ou rejeitar o número de telefone dos EUA com base em qualquer combinação dos formatos fornecidos acima. O código de área é obrigatório. Se o código do país for fornecido, você deverá confirmar que o código 
+do país é 1. Retorna truese a string for um número de telefone dos EUA válido; caso contrário, retorne false. */
+
+
+  function telephoneCheck(str) {
+    // regex that matches a phone number with no spaces,
+    // with spaces, with dashes, and with or without country code
+    let regex = /^[1]*[\s|-]*\d{3}[\s|-]*\d{3}[\s|-]*\d{4}$/;
+  
+    // regex that matches parentheses
+    // with no spaces, with spaces, with dashes,
+    // and with or without country code
+    let regexParentheses = /^[1]*[\s]*[(]\d{3}[)][\s]*\d{3}[\s|-]*\d{4}$/;
+  
+    if (!regex.test(str)) {
+      return regexParentheses.test(str);
+    } else {
+      return true;
+    }
+  
+  }
+
+  //outra solução
+
+  var re = /^([+]?1[\s]?)?((?:[(](?:[2-9]1[02-9]|[2-9][02-8][0-9])[)][\s]?)|(?:(?:[2-9]1[02-9]|[2-9][02-8][0-9])[\s.-]?)){1}([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2}[\s.-]?){1}([0-9]{4}){1}$/;
+
+function telephoneCheck(str) {
+  return re.test(str);
+}
+
+
+//Caixa registradora
+/* Projete uma função de gaveta de caixa registradora checkCashRegister()que aceite preço de compra como primeiro argumento ( price), pagamento como segundo argumento ( cash) e caixa registradora ( cid) como terceiro argumento.
+
+cidé uma matriz 2D listando a moeda disponível.
+
+A checkCashRegister()função deve sempre retornar um objeto com uma statuschave e uma changechave.
+
+Devolva {status: "INSUFFICIENT_FUNDS", change: []}se o dinheiro na gaveta for menor que o troco devido ou se você não puder devolver o troco exato.
+
+Devolva {status: "CLOSED", change: [...]}com caixa como valor da chave changese for igual ao troco devido.
+
+Caso contrário, retorne {status: "OPEN", change: [...]}, com o troco devido em moedas e cédulas, ordenados da ordem mais alta para a mais baixa, conforme o valor da changechave. */
+
+const CLOSED = "CLOSED";
+const INSUFFICIENT_FUNDS = "INSUFFICIENT_FUNDS";
+const OPEN = "OPEN";
+
+const PENNY = 0;
+const NICKEL = 1;
+const DIME = 2;
+const QUARTER = 3;
+const ONE = 4;
+const FIVE = 5;
+const TEN = 6;
+const TWENTY = 7;
+const ONE_HUNDRED = 8;
+
+const currUnits = [
+  ["PENNY", 0.01], 
+  ["NICKEL", 0.05], 
+  ["DIME", 0.1], 
+  ["QUARTER", 0.25], 
+  ["ONE", 1], 
+  ["FIVE", 5], 
+  ["TEN", 10], 
+  ["TWENTY", 20], 
+  ["ONE HUNDRED", 100]
+]
+
+function checkCashRegister(price, cash, cid) {
+  var change;
+  let changeDue = cash - price;
+  const drawerStatus = getDrawerStatus(changeDue, cid);
+
+  switch(drawerStatus){
+    case INSUFFICIENT_FUNDS:
+      change = { status: INSUFFICIENT_FUNDS, change: [] }
+      break;
+    case CLOSED:
+      change = { status: CLOSED, change: [...cid] }
+      break;
+    case OPEN:
+      const changeCurrency = [...collectChange(changeDue, cid)];
+      change = changeCurrency.length > 0 ? { status: OPEN, change: changeCurrency } : { status: INSUFFICIENT_FUNDS, change: [] }
+  }
+  
+  // Here is your change, ma'am.
+  return change;
+}
+
+function getDrawerStatus(changeDue, cashInDrawer){
+  let totalCash = 0;
+  let status;
+  for(let i = 0; i < cashInDrawer.length; i++){
+    totalCash += cashInDrawer[i][1];
+  }
+  if(totalCash > changeDue){
+    status = "OPEN";
+  }
+  if(totalCash == changeDue){
+    status = "CLOSED";
+  }
+  if(totalCash < changeDue){
+    status = "INSUFFICIENT_FUNDS";
+  }
+  return status;
+}
+
+function collectChange(changeDue, cashInDrawer) {
+  const changeBreakdown = splitChange(changeDue); 
+  const { dollars, cents } = changeBreakdown;
+  const billChange = getBillsFromDrawer(dollars, cashInDrawer).change;
+  let coinChange = getCoinsFromDrawer(cents, cashInDrawer).change;
+  return billChange.concat(coinChange);
+}
+
+function splitChange(change){
+  const breakdown = change.toString().split(".");
+  return { dollars: breakdown[0], cents: breakdown[1] }
+}
+
+function splitBillAmounts(amount){
+  let digits = amount.toString().split('').reverse();
+  
+  for(let i = 0; i < digits.length; i++){
+     digits[i] =  i >= 1 ? parseInt(digits[i]+'0'.repeat(i)) : parseInt(digits[i]);
+  }
+  return digits.reverse();  
+}
+
+function selectBillOrCoinChange(amount, cid, unit, change){
+  let currency = cid[unit];
+  if(unit < PENNY){
+       return { change: [], cid }
+  }
+  else if(amount == 0){
+    return { change, cid };
+  }
+  else if(amount <= currency[1]){
+    if(amount%currUnits[unit][1] != 0){
+      const amtToRemove = Math.floor(amount/currUnits[unit][1])*currUnits[unit][1];
+      cid[unit] = [currency[0], currency[1] - amtToRemove]
+      change.push([currency[0], amtToRemove])
+      const remAmount = unit > QUARTER ? amount - amtToRemove : parseFloat((amount - amtToRemove).toFixed(2))
+      return selectBillOrCoinChange(remAmount, cid, unit - 1, change)
+    } else {
+      cid[unit] = [currency[0], currency[1] - amount]  
+      change.push([currency[0], amount])
+    }
+    
+    return { change, cid }
+  } 
+  else if(amount > currency[1]){
+      const remAmount = amount - currency[1];
+      if(currency[1] != 0){     
+        change.push(currency);
+      }
+      cid[unit] = [currency[0], 0]
+      return selectBillOrCoinChange(remAmount, cid, unit - 1, change);
+  }
+}
+
+function getBillsFromDrawer(amount, cid){
+   const billAmounts = splitBillAmounts(amount);
+   let result = [];
+   
+   let i = 0, change = [];
+   while(i < billAmounts.length){
+     if(billAmounts[i] >= 100) {
+        result.push(selectBillOrCoinChange(billAmounts[i], cid, ONE_HUNDRED, change));
+        i++
+        continue;
+     }
+     if(billAmounts[i] >= 20){
+       result.push(selectBillOrCoinChange(billAmounts[i], cid, TWENTY, change));
+       i++
+       continue;
+     }
+     
+     if(billAmounts[i] >= 10){
+       result.push(selectBillOrCoinChange(billAmounts[i], cid, TEN, change));
+       i++
+       continue;
+     }
+     if(billAmounts[i] >= 5){
+       result.push(selectBillOrCoinChange(billAmounts[i], cid, FIVE, change));
+       i++
+       continue;
+     }
+     result.push(selectBillOrCoinChange(billAmounts[i], cid, ONE, change));
+     i++;
+   }
+   result = result[result.length-1];
+   result.change = arrangeChange(result.change);
+   return result
+}
+
+function getCoinsFromDrawer(amount, cid){
+  const coinAmounts = splitCoinAmounts(amount);
+   let result = [];
+   
+   let i = 0, change = [];
+   while(i < coinAmounts.length){
+     if(coinAmounts[i] >= 0.25) {
+        result.push(selectBillOrCoinChange(coinAmounts[i], cid, QUARTER, change));
+        i++
+        continue;
+     }
+     
+     if(coinAmounts[i] >= 0.1) {
+        result.push(selectBillOrCoinChange(coinAmounts[i], cid, DIME, change));
+        i++
+        continue;
+     }
+     
+     if(coinAmounts[i] >= 0.5) {
+        result.push(selectBillOrCoinChange(coinAmounts[i], cid, NICKEL, change));
+        i++
+        continue;
+     }
+     result.push(selectBillOrCoinChange(coinAmounts[i], cid, PENNY, change));
+     i++
+   }
+   result = result[result.length-1];
+   result.change = arrangeChange(result.change);
+   return result
+}
+
+function splitCoinAmounts(amount) {
+  let digits = amount.toString().split('');
+  
+  for(let i = 0; i < digits.length; i++){
+     digits[i] =  i >= 1 ? parseFloat('0.'+'0'.repeat(i)+digits[i]) : parseFloat('0.'+digits[i]);
+  }
+  return digits;  
+}
+
+// Utility Functions
+function arrangeChange(change){
+  let arrangedChange = [];
+  let copyChange = change;
+  for(let i = 0; i < copyChange.length; i++){
+    let currency = copyChange[i];
+    if(existsInArr(arrangedChange, currency[0])) {
+       continue; 
+    }
+    for(let j = 0; j < change.length; j++){
+      if(i != j) {
+        if(currency[0] == change[j][0]){
+          currency[1] += change[j][1];
+        }
+      }
+    }
+    arrangedChange.push(currency)
+  }
+  return arrangedChange;
+}
+
+function existsInArr(haystack, needle) {
+    for (var i = 0; i < haystack.length; ++i) {
+        if (haystack[i][0] == needle) {
+            return true;
+        }
+    }
+    return false;
+}
