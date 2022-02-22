9c7866656604c65bf15840924b6a4f45a2a9bf1e
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-02-22 10:03:08 -0300

    Update Script de algoritmo intermediário.js

--- FreeCodeCamp/Algoritmos JavaScript e Estruturas de Dados/Script de algoritmo intermediário.js
+++ FreeCodeCamp/Algoritmos JavaScript e Estruturas de Dados/Script de algoritmo intermediário.js
@@ -480,3 +480,29 @@ const Person = function(firstAndLast) {
 const bob = new Person('Bob Ross');
 bob.setFirstName("victor")
 console.log(bob.getFirstName());
+
+//Mapeie os detritos
+//Retorna um novo array que transforma a altitude média dos elementos em seus períodos orbitais (em segundos).
+
+function orbitalPeriod(arr) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  const a = 2 * Math.PI;
+  const newArr = [];
+
+  const getOrbPeriod = function(obj) {
+    const c = Math.pow(earthRadius + obj.avgAlt, 3);
+    const b = Math.sqrt(c / GM);
+    const orbPeriod = Math.round(a * b);
+    // create new object
+    return {name: obj.name, orbitalPeriod: orbPeriod};
+  };
+
+  for (let elem in arr) {
+    newArr.push(getOrbPeriod(arr[elem]));
+  }
+
+  return newArr;
+}
+
+//
