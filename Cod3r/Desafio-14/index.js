e7618ff2a6b28f86705a52e9de43e839cb9d3f81
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-03-02 18:25:22 -0300

    Create index.js

---
+++ Cod3r/Desafio-14/index.js
@@ -0,0 +1,35 @@
+/* - 14: Crie uma estrutura condicional switch que receba uma string com o nome de uma fruta e que possua três
+casos: Caso maçã, retorne no console: “Não vendemos esta fruta aqui”. Caso kiwi, retorne: “Estamos com
+escassez de kiwis”. Caso melancia, retorne: “Aqui está, são 3 reais o quilo”. Teste com estas três opções .Crie
+também um default, que retornará uma mensagem de erro no console. */
+
+function frutas(fruta){
+    switch (fruta) {
+        case "maçã":
+            return `Não vendemos esta fruta aqui`
+        case "kiwi":
+            return `stamos com
+            escassez de kiwis`
+        case "melancia":
+            return `Aqui está, são 3 reais o quilo`
+        default:
+            return `Error, fruta não encontrada`
+      }
+}
+
+let verfrutas = (fruta) => {
+    switch (fruta) {
+        case "maçã":
+            return `Não vendemos esta fruta aqui`
+        case "kiwi":
+            return `stamos com
+            escassez de kiwis`
+        case "melancia":
+            return `Aqui está, são 3 reais o quilo`
+        default:
+            return `Error, fruta não encontrada`
+      }
+}
+
+console.log(frutas("melancia"))
+console.log(verfrutas("banana"))
