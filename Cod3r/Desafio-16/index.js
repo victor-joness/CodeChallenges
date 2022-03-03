e0ee1f353a0c993396a47763fb366e5d79f22639
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-03-03 19:29:36 -0300

    Create index.js

---
+++ Cod3r/Desafio-16/index.js
@@ -0,0 +1,33 @@
+/* - 16: Utilizando a estrutura do Switch faça um programa que simule uma calculadora básicaO programa recebe
+como parâmetros dois valores numéricos e uma string referente à operação e a realize com os valores
+numéricos na ordem que foram inseridos. Por exemplo: calculadora (2, ‘+’, 3). A função efetuará a soma de 2 e
+3. Dica: Os sinais das operações são: ‘+’. ‘-’, ‘*’ e ‘/’. Crie um caso default para operações inválidas. */
+
+function calculadora(num, operador, num2){
+    if( typeof operador !== "string"){
+        return "Uma das entradas é invalida"
+    }
+
+    if( typeof num !== "number"){
+        return "Uma das entradas é invalida"
+    }
+
+    if( typeof num2 !== "number"){
+        return "Uma das entradas é invalida"
+    }
+
+    switch(operador){
+        case "+": 
+            return num + num2
+        case "-": 
+            return num - num2
+        case "*":
+            return num * num2
+        case "/":
+            return num / num2
+        default:
+            return "operações inválida."
+    }
+}
+
+console.log(calculadora(2,"+",3))
