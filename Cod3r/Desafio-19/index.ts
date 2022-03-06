25ff7bd0a596ffdcc843be7a4e381fddafd00e2d
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-03-06 15:01:09 -0300

    Create index.ts

---
+++ Cod3r/Desafio-19/index.ts
@@ -0,0 +1,22 @@
+/* Implemente uma função que receba como parâmetros o código do item pedido, a quantidade e calcule o valor
+a ser pago por aquele lanche. Considere que a cada execução somente será calculado um item. Use o
+comando switch. Crie um caso default para produto não existente. */
+
+function mercadinho2(codigo: number, quantidade: number){
+    switch(codigo){
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10: return `${quantidade} unidades fica ${quantidade * codigo} reais`
+        default: return "Não temos esse item"
+    }
+
+}
+
+console.log(mercadinho2(2, 10));
