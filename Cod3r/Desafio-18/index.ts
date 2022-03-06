efd40dabd1cd1dc7e43a653cb014d2bfc3c9012e
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-03-06 15:01:05 -0300

    Create index.ts

---
+++ Cod3r/Desafio-18/index.ts
@@ -0,0 +1,23 @@
+/* Faça um programa que leia um número entre 0 e 10, e escreva este número por extenso. Use o comando
+switch. Crie um case default que escreva ‘Número fora do intervalo.’ */
+
+function listagem2(numero: number){
+    if(numero < 0 || numero > 10){
+        return "numero invalido"
+    }
+
+    switch(numero){
+        case 1: return "Um"
+        case 2: return "Dois"
+        case 3: return "Três"
+        case 4: return "Quatro"
+        case 5: return "Cinco"
+        case 6: return "Seis"
+        case 7: return "Sete"
+        case 8: return "Oito"
+        case 9: return "Nove"
+        case 10: return "Dez"
+    }
+}
+
+console.log(listagem2(4))
