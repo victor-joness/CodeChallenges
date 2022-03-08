40313ab2be2b2506c931f15ed6f7089ad597437d
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-03-08 20:21:54 -0300

    Create index.js

---
+++ Cod3r/Desafio-25/index.js
@@ -0,0 +1,11 @@
+/* Escrever um programa para exibir os números de 1 até 50 na tela. */
+
+function numeros(){
+    let i = 1;
+    while(i <= 50){
+        console.log(i);
+        i++
+    }
+}
+
+numeros()
