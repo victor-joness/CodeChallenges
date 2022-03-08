f4c22ad7cb50d4f1afb15d47ea031a0f0b1710c9
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-03-08 20:21:52 -0300

    Create index.ts

---
+++ Cod3r/Desafio-24/index.ts
@@ -0,0 +1,11 @@
+/* Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while. */
+
+function imprimir2(){
+    let i = 0;
+    while(i < 11){
+        console.log("Hello World!");
+        i++;
+    }
+}
+
+imprimir2()
