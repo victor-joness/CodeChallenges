8c53e83f29c9153eb575a72363ebff9faa5a2e35
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-03-11 09:27:10 -0300

    Create index.js

---
+++ Cod3r/Desafio-32/index.js
@@ -0,0 +1,19 @@
+/* Construir um algoritmo que calcule a média aritmética dos valores de um vetor de inteiros */
+
+function media(vetor){
+    let quantidade = vetor.length;
+    let soma = 0;
+
+    for(let i = 0; i < quantidade; i++){
+        soma = soma + vetor[i]
+    }
+
+    let media = soma / quantidade;
+
+    return `A media é de ${media.toFixed(2).replace(".", ",")}`
+
+}
+
+let vetor = [2,4,5,6,10,13,15,20];
+
+console.log(media(vetor));
