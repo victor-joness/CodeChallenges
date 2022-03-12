6b3692554deba47207928269d8e4da6b9d59bf31
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-03-12 17:26:39 -0300

    Create index.js

---
+++ Cod3r/Desafio-34/index.js
@@ -0,0 +1,25 @@
+/* Construa uma função que receberá duas Strings de tamanhos variados e que retornará True ou False caso
+todos os caracteres (independentemente de ser maiúsculo ou minúsculo) estejam contidos em ambas palavras. */
+
+function verificar(string1, string2){
+    let str1 = string1.toLowerCase();
+    let str2 = string2.toLowerCase();
+
+    let contador = 0;
+
+    for(let i = 0; i < str1.length; i++){
+        for(let j = 0; j < str2.length; j++){
+            if(str1[i] == str2[j]){
+                contador++
+            }
+        }
+    }
+
+    if(contador >= str1.length){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(verificar("victro", "ovictr"));
