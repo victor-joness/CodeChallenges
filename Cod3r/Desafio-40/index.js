ad3e9736db5a753ee7eadaae09bf984fc361fc4a
Victor Mesquita <victorjonesmesquits@gmail.com>
2022-03-14 15:22:22 -0300

    Create index.js

---
+++ Cod3r/Desafio-40/index.js
@@ -0,0 +1,24 @@
+/*  Faça uma função que receba como parâmetro um vetor de notas e mostre os conceitos de cada uma de
+modo que de 0,0 a 4,9 seja atribuído o conceito D, de 5,0 a 6,9 seja atribuído o conceito C, de 7,0 a 8,9 o
+conceito B e de 9,0 a 10,0 o conceito A. */
+
+function notas(vetor){
+    let resultado = []
+    for(let i = 0; i< vetor.length; i++){
+        if(vetor[i] > 0 && vetor[i] <= 4.9){
+            resultado.push("Nota D");
+        }else if(vetor[i] > 4.9 && vetor[i] <= 6.9){
+            resultado.push("Nota C");
+        }else if(vetor[i] > 6.0 && vetor[i] < 8.9){
+            resultado.push("Nota B");
+        }else if(vetor[i] > 8.9 && vetor[i] < 10){
+            resultado.push("Nota A")
+        }
+    }
+
+    return resultado;
+}
+
+let notass = [3.5,6,7.2,9.6];
+
+console.log(notas(notass))
