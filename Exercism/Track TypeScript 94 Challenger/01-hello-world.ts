d4fe614e9f1a0edbfade357b13a52023f53909d2
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-03-16 18:12:55 -0300

    Create 01-hello-world.ts

---
+++ Exercism/Track TypeScript 94 Challenger/01-hello-world.ts
@@ -0,0 +1,16 @@
+/* Os objetivos são simples:
+
+Escreva uma função que reembolsa uma string "Hello, World!".
+Execute o conjunto de testes e certifique-se de que ele seja bem sucedido.
+Envie sua solução e confira no site.
+ */
+
+function hello(){
+    return "hello world"
+}
+
+console.log(hello());
+
+export function hello2(): string {
+    return "Hello, World!"
+}
