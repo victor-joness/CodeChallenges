a13584331c48b6fca01e0628f12d2436c12e67ec
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-04-10 10:56:55 -0300

    Create index.js

---
+++ Cod3r/Desafio-41/index.js
@@ -0,0 +1,17 @@
+/* 
+Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a
+função) e com ponto de exclamação "!" no final.
+
+
+Exemplos:
+cumprimentar("Leonardo") // retornará "Olá, Leonardo!"
+cumprimentar("Maria") // retornará "Olá, Maria!"
+
+*/
+
+function cumprimentar(nome){
+    return `Olá, ${nome}!`
+};
+
+console.log(cumprimentar("Leonardo"));
+console.log(cumprimentar("Maria"));
