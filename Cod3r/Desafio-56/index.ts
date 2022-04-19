74f1d220d7aa52f396817064eb929a4375e52913
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-04-19 09:53:41 -0300

    Create index.ts

---
+++ Cod3r/Desafio-56/index.ts
@@ -0,0 +1,32 @@
+/* 
+A fim de manter o calendário anual ajustado com o movimento de translação da Terra, criou-se os anos
+bissextos, que têm 366 dias em vez dos 365 presentes nos anos normais.
+Para determinar se um ano é bissexto, é necessário saber se ele é multiplo de 4. Não pode ser múltiplo de 100,
+exceto se for também múltiplo de 400.
+Com isso em mente, desenvolva uma função que recebe um número correspondente a um ano e retorna se ele
+é bissexto ou não.
+*/
+
+const checarAnoBissexto2 = (ano:number) => {
+    if(ano % 4 == 0){
+        if(ano % 100 != 0){
+            return true;
+        }else{
+            if(ano % 400 == 0){
+                return true;
+            }
+            return false;
+        }
+    }
+    return false;
+ }
+ 
+ console.log(checarAnoBissexto2(2020));
+ console.log(checarAnoBissexto2(2100));
+ console.log(checarAnoBissexto2(2015));
+ console.log(checarAnoBissexto2(2016));
+ console.log(checarAnoBissexto2(1500));
+ console.log(checarAnoBissexto2(1600));
+ console.log(checarAnoBissexto2(1700));
+ console.log(checarAnoBissexto2(2000));
+ console.log(checarAnoBissexto2(2012));
