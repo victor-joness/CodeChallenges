e76115c50e6617c17e5a359cd7dcf28a252a11d4
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-04-19 09:53:15 -0300

    Create index.ts

---
+++ Cod3r/Desafio-55/index.ts
@@ -0,0 +1,19 @@
+/* 
+Elabore uma função que receba um array de números e retorne um array que tenha todos os números que são
+pares e que também tenham índices pares.
+*/
+
+const receberSomenteOsParesDeIndicesPares2 = (array:Array<number>) => {
+    let resultado = [];
+
+    for(let i = 0; i < array.length; i++){
+        if(array[i] % 2 == 0 && i % 2 == 0){
+            resultado.push(array[i])
+        }
+    }
+
+    return resultado
+}
+
+console.log(receberSomenteOsParesDeIndicesPares2([1, 2, 3, 4]))
+console.log(receberSomenteOsParesDeIndicesPares2([10, 70, 22, 43]))
