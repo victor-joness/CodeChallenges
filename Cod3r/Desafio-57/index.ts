6d8dd1a3d0b4a28ac058f32345d148fd148f6fcd
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-04-25 09:35:09 -0300

    Create index.ts

---
+++ Cod3r/Desafio-57/index.ts
@@ -0,0 +1,14 @@
+/* 
+Escreva uma função que receba um array de números e retornará a soma de todos os números desse array.
+*/
+
+//reduce vai passar por todos os elementos do array e ir armazenando eles no acumulador, na ultimo chamada que temos o return
+// o acumulador por padrao começa com 0;
+
+const somarNumeros2 = (array: Array<number>) => {
+    return array.reduce((acumulador, valorAtual) => acumulador + valorAtual)
+}
+
+console.log(somarNumeros2([10, 10, 10]));
+console.log(somarNumeros2([15, 15, 15, 15]));
+console.log(somarNumeros2([10, 5, 2, 15]));
