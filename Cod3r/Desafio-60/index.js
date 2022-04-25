5b61718914e71a186733fafdfd617b65f31ede55
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-04-25 09:35:33 -0300

    Create index.js

---
+++ Cod3r/Desafio-60/index.js
@@ -0,0 +1,12 @@
+/* 
+Faça uma função que recebe a base e a altura de um triângulo e retorne a área desse triângulo. A precisão
+deverá ser de duas casas decimais, arredondando se necessário.
+*/
+
+const areaDoTriangulo = (base, altura) => {
+    return ((base * altura) / 2).toPrecision(4);
+}
+
+console.log(areaDoTriangulo(10, 15));
+console.log(areaDoTriangulo(7, 9));
+console.log(areaDoTriangulo(9.25, 13.1));
