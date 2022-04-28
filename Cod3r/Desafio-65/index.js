d98b55f8cde1727fc297b46f6547a8ee42a5e000
Victor Mesquita <85620625+victor-joness@users.noreply.github.com>
2022-04-28 10:20:06 -0300

    Create index.js

---
+++ Cod3r/Desafio-65/index.js
@@ -0,0 +1,22 @@
+/* 
+A fim de criar um mecanismo de busca para sua aplicação, você precisa iniciar criando uma função para
+identificar palavras semelhantes.
+Escreva uma função que recebe como primeiro parâmetro uma palavra e, como segundo parâmetro, um array
+de strings. A função deverá filtrar as palavras do array que contêm nelas a string do primeiro parâmetro.
+*/
+
+const buscarPalavrasSemelhantes = (string, array) => {
+    let resultado = []
+    for(let i = 0; i < array.length; i++){
+        if(array[i].includes(string)){
+            resultado.push(array[i])
+        }
+    }
+
+    return resultado;
+
+    /* return array.filter((elemento) => elemento.includes(string)); */
+}
+
+console.log(buscarPalavrasSemelhantes("pro", ["programação", "mobile", "profissional"]));
+console.log(buscarPalavrasSemelhantes("python", ["javascript", "java", "c++"]));
