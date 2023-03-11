44b1832a146f1b7e75ae3e515ddf9127d63cb058
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-03-11 14:25:37 -0300

    Create Desafio-1004.js

---
+++ Beecrowd/Iniciantes/Desafio-1004.js
@@ -0,0 +1,20 @@
+/* 
+Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. 
+A seguir mostre a variável PROD com mensagem correspondente.
+*/
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+
+var a = parseInt(lines.shift());
+var b = parseInt(lines.shift());
+
+var PROD = a * b;
+
+console.log("PROD = " + PROD);
