bc67750ad33371b3b1ecc564997cea8a7ae0c687
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-03-12 09:44:52 -0300

    Create Desafio-1017.js

---
+++ Beecrowd/Iniciantes/Desafio-1017.js
@@ -0,0 +1,19 @@
+/* 
+Joaozinho quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem, ao utilizar um automóvel que faz 12 KM/L. Para isso, ele gostaria que você o auxiliasse através de um simples programa. Para efetuar o cálculo, deve-se fornecer o tempo gasto na viagem (em horas) e a velocidade média durante a mesma (em km/h). Assim, pode-se obter distância percorrida e, em seguida, calcular quantos litros seriam necessários. Mostre o valor com 3 casas decimais após o ponto.
+*/
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+
+var primeiro = parseInt(lines.shift());
+var segundo = parseInt(lines.shift());
+
+var gasolina = (primeiro * segundo) / 12;
+
+console.log(gasolina.toFixed(3));
