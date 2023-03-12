c2e58ff67aa0e4ad58d979564e4b68d08728b9f6
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-03-12 09:44:35 -0300

    Create Desafio-1011.js

---
+++ Beecrowd/Iniciantes/Desafio-1011.js
@@ -0,0 +1,23 @@
+/* 
+Faça um programa que calcule e mostre o volume de uma esfera sendo fornecido o valor de seu raio (R). A fórmula para calcular o volume é: (4/3) * pi * R3. Considere (atribua) para pi o valor 3.14159.
+
+Dica: Ao utilizar a fórmula, procure usar (4/3.0) ou (4.0/3), pois algumas linguagens (dentre elas o C++), assumem que o resultado da divisão entre dois inteiros é outro inteiro.
+
+
+*/
+
+/* var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */ 
+/* var raio = parseFloat(lines.shift());
+var pi = 3.14159;
+var volume = parseFloat(((4.0/3) * pi * (raio*raio*raio)))
+
+console.log("VOLUME = " + volume.toFixed(3)); */
+
+console.log(parseInt(587/100))
